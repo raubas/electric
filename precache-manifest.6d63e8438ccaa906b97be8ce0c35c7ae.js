61aac1be268a4c74f487e2879ec8355b69c294c6
Ruben <rs@artworks.io>
2019-02-22 15:57:58 +0100

    Updates

---
+++ precache-manifest.6d63e8438ccaa906b97be8ce0c35c7ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bebf23b58d1971e82ca",
+    "url": "/electric/static/css/main.c4ec11df.chunk.css"
+  },
+  {
+    "revision": "1bebf23b58d1971e82ca",
+    "url": "/electric/static/js/main.1bebf23b.chunk.js"
+  },
+  {
+    "revision": "3f608a0f182832fe3dbd",
+    "url": "/electric/static/js/runtime~main.3f608a0f.js"
+  },
+  {
+    "revision": "608742d1cac983c1ce7e",
+    "url": "/electric/static/js/2.608742d1.chunk.js"
+  },
+  {
+    "revision": "8fec284d72adbae47c8cb1d73c80a1ae",
+    "url": "/electric/index.html"
+  }
+];
